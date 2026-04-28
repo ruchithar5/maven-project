abf64141114457f83f568357ee960758097522a3
Ruchitha R <ruchithareddy25042005@gmail.com>
2026-04-28 05:18:22 +0000

    changes done

---
+++ src/main/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testAdd() {
+        assertEquals(4, 2 + 2);
+    }
+}
